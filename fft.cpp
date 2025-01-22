98265f6edefba74dc69bac430a2348f4453d6cf7
auah <2905761372@qq.com>
2025-01-22 22:29:58 +0800

    三种模式归一

--- fft.cpp
+++ fft.cpp
@@ -5,29 +5,6 @@
 
 namespace FFTLibrary {
 
-    // FFTColumn：按列计算 FFT
-    Eigen::MatrixXcd FFTColumn::fft(const Eigen::MatrixXcd& X, int n, int dim) const {
-        Eigen::FFT<double> fft;
-        Eigen::MatrixXcd Y(X.rows(), X.cols());
-
-        if (dim == 1) {
-            // 对每一列进行 FFT 计算
-            for (int i = 0; i < X.cols(); ++i) {
-                // 获取列，并将其从实数转换为复数
-                Eigen::VectorXd x_col_real = X.col(i).real();
-                Eigen::VectorXcd x_col = x_col_real.cast<std::complex<double>>();  // Cast to complex
-
-                // 创建复数输出向量
-                Eigen::VectorXcd y_col(n);
-
-                // 执行 FFT
-                fft.fwd(y_col, x_col);
-                Y.col(i) = y_col;
-            }
-        }
-        return Y;
-    }
-
     // FFTN：指定维度计算 FFT
     Eigen::MatrixXcd FFTN::fft(const Eigen::MatrixXcd& X, int n, int dim) const {
         Eigen::FFT<double> fft;
@@ -67,8 +44,8 @@ namespace FFTLibrary {
 
     // 公共接口：按列计算
     Eigen::MatrixXcd fft(const Eigen::MatrixXcd& X) {
-        FFTColumn fftColumn;
-        return fftColumn.fft(X);
+        FFTN fftN;
+        return fftN.fft(X, X.cols(), 1);
     }
 
     // 公共接口：按列计算，n 点 FFT
